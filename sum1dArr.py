30611dcfcd1675bdc09a5b950bb6c5871500e8e9
affanfarid <affanfarid29@gmail.com>
2020-09-18 00:00:15 -0500

    finished sum1darr

---
+++ sum1dArr.py
@@ -0,0 +1,37 @@
+'''
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+
+'''
+
+def sum1dArr(arr):
+    finalArr = []
+    for x in range(len(arr)):
+        if arr[x] == 0:
+            finalArr.append(arr[x])
+        else:
+            finalArr.append(arr[x-1]+arr[x])
+    return finalArr
+
+        
