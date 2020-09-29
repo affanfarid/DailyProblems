e00a91985ff3f3e2e4593ba778a5db04fecd6cc8
affanfarid <affanfarid29@gmail.com>
2020-09-28 23:49:42 -0500

    finished appendAndDelete

--- appendAndDelete/appendAndDelete.py
+++ appendAndDelete/appendAndDelete.py
@@ -1,4 +1,26 @@
 def appendAndDelete(s,t,k):
-    for x in s:
-        
-    return 
+    sNum = len(s)
+    totalNum = 0
+    for x in range(len(s)):
+        if s[:x] in t:
+            continue
+        else:
+            sNum = x - 1
+            break
+    #totalNum += len(s) - sNum
+    print(s[:sNum])
+    t = t.replace(s[:sNum],'')
+    print(t)
+    totalNum = len(t) + len(s) - sNum
+
+    print(totalNum)
+    return totalNum == k
+
+
+s1 = "hackerhappy"
+t1 = "hackerrank"
+print(appendAndDelete(s1, t1, 9))
+
+s2 = "hacker"
+t2 = "hackerrank"
+print(appendAndDelete(s2, t2, 4))
