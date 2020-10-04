2f35f127415b1f5f9e6119734374a9cdf4a83032
affanfarid <affanfarid29@gmail.com>
2020-10-03 23:49:59 -0500

    still working

--- maximizeXor/maximizeXor.py
+++ maximizeXor/maximizeXor.py
@@ -2,11 +2,12 @@ def maximizeXor(l,r):
     maxXor = 0
     for x in range(l,r+1):
         for y in range(x,r+1):
-            num1 = bin(x)
-            num2 = bin(y)
-            binNum = bin(num1 ^ num2)
+            #num1 = '0b' + bin(x)
+            #num2 = '0b' + bin(y)
+            binNum = bin(bin(x) ^ bin(y))
             xorVal = int(binNum,2)
             maxXor = max(maxXor, xorVal)
 
     return maxXor
 
+print(maximizeXor(11,100))
